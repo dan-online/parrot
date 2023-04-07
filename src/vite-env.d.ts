45883da0fb4b3d6035b8d64f9ba48a2fe944c7d7
DanCodes <dan@dancodes.online>
2023-04-07 13:03:03 +0100

    chore(unplugin): use types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,9 +5,3 @@ declare module "*.vue" {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
-
-declare module "~icons/*" {
-  import type { DefineComponent } from "vue";
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
-}
